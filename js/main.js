3839ad4900d6617a9d9707e115898e2494fdc647
kiyo0811 <kiyo0811@naver.com>
2022-05-20 01:32:06 +0900

    숫자 맞추기 게임 기능구현1

--- js/main.js
+++ js/main.js
@@ -11,7 +11,8 @@
 let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
-let resultArea = document.getElementById("result-area");
+let resultText = document.getElementById("result-text");
+let resultAreaImg = document.querySelector(".main-img")
 let resetButton = document.getElementById("reset-button");
 let chances = 5;
 let gameOver = false;
@@ -33,11 +34,11 @@ function play(){
 
     // 유효성 검사
     if(userValue<1 || userValue>100){
-        resultArea.textContent = "1과 100사이 숫자를 입력해주세요";
+        resultText.textContent = "1과 100사이 숫자를 입력해주세요";
         return;
     }
     if(history.includes(userValue)){
-        resultArea.textContent="이미 입력한 숫자입니다. 다른 숫자를 입력해주세요.";
+        resultText.textContent="이미 입력한 숫자입니다. 다른 숫자를 입력해주세요.";
         return;
     }
 
@@ -45,11 +46,14 @@ function play(){
     chanceArea.textContent = `남은기회:${chances}번`;
 
     if(userValue < computerNum){
-        resultArea.textContent = "Up!!"
+        resultAreaImg.src = "./images/up.jpg";
+        resultText.textContent = "Up!!";
     }else if(userValue > computerNum){
-        resultArea.textContent = "Down!!"
+        resultAreaImg.src = "./images/down.jpg";
+        resultText.textContent = "Down!!"
     }else{
-        resultArea.textContent = "정답입니다!!"
+        resultAreaImg.src = "./images/answer.jpg";
+        resultText.textContent = "정답입니다!!"
         gameOver = true;
     }
 
@@ -70,7 +74,7 @@ function reset(){
     // user input창이 깨끗하게 정리
     userInput.value = "";
     // 결과창문구 초기화
-    resultArea.textContent = "결과창";
+    resultText.textContent = "결과창";
     // 남은기회 최기화
     gameOver = false;
     playButton.disabled = false;
